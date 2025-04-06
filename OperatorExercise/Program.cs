d50982a2789415c751987530aa12e4c5eb380f8a
Parker Sims <parker4al@gmail.com>
2025-04-06 18:22:44 -0500

    Completed exercise 1

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            int sum = a + b;
+            int difference = a - b;
+            int product = a * b;
+            int quotient = a / b;
+            int remainder = a % b;
+            
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}.");
         }
     }
 }
